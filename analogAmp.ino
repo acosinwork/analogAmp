65c9c53951b49abc935eeb9e199d8f217000629b
acosinwork <acosinwork@yandex.ru>
2014-10-24 00:14:44 +0400

    first commit

---
+++ analogAmp.ino
@@ -0,0 +1,13 @@
+// Здесь назначаем пины
+#include "pinMap.h"
+//Здесь работаем с громкостью
+#include "digiPot.h"
+
+int a = IR_PIN;
+void setup()
+{
+}
+
+void loop()
+{
+}
---
+++ pinMap.h
@@ -0,0 +1,37 @@
+// Здесь мы назначаем функции всем пинам
+// ИК-порт
+#define IR_PIN     2
+
+// Реле
+#define RELAY_PIN  3
+
+// Цифровой потенциометр
+#define D_POT_MOSI 4
+#define D_POT_SCK  5
+#define D_POT_CS   6
+
+// Экранчик
+#define LCD_RS     7
+#define LCD_E      8
+#define LCD_D4     9
+#define LCD_D5     10
+#define LCD_D6     11
+#define LCD_D7     12
+
+// обычными потенциометрами тоже можно 
+// будет всё регулировать. 
+// Они же задают громкость при включении
+#define VOLUME_POT  A3
+#define LOW_POT     A4
+#define HIGH_POT    A5
+
+// на будущее - peakLevel
+
+#define AIN_LEFT    A0
+#define AIN_RIGHT   A1
+#define PEAK_LEFT   A2
+#define PEAK_RIGHT  13
+
+
+
+
