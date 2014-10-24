9a05ab84a27466ea80ba312526594764dde4bb51
acosinwork <acosinwork@yandex.ru>
2014-10-24 23:02:52 +0400

    add volume

---
+++ volume.h
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+
+
+#define REG_VOL  0
+#define REG_LOW  1
+#define REG_HIGH 2
+
+
+class Regulator {
+
+  public:
+  
+    setPotent
+  
+  private:
+  
+    uint8_t analogInput;
+    
+  
+
+};
